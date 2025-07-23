4317b369fbbb43908ba2041e9c7be2a7c74b63da
Nishit Lugun <lugun.nishit@gmail.com>
2025-07-24 03:37:42 +0530

    sdsdsdsdsd

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,11 +16,8 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Fight Arena",
   description: "Fight till you die",
-  other: {
-    "http-equiv": "Cache-Control, Pragma, Expires",
-    content: "no-cache, no-store, must-revalidate, no-cache, 0",
-  },
 };
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
