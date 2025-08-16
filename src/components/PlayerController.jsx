2a78921e5eeaa5ba26675195a4e4729cea0889d5
Nishit Lugun <lugun.nishit@gmail.com>
2025-08-17 03:28:49 +0530

    kdkdk

--- src/components/PlayerController.jsx
+++ src/components/PlayerController.jsx
@@ -327,6 +327,7 @@ const PlayerController = forwardRef(
       if (!socket) return;
 
       const onPlayerHit = (data) => {
+        // console.log("onPlayerHit received", data);
         if (data.victimId === playerId) {
           takeHit(data.attackType, data.attackTime);
         }
