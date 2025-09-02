d69168d812b50838fe3706a1d452d1505b133e49
Nishit Lugun <lugun.nishit@gmail.com>
2025-09-03 02:11:01 +0530

    nmsjkds

--- src/components/PlayerController.jsx
+++ src/components/PlayerController.jsx
@@ -609,7 +609,7 @@ const PlayerController = forwardRef(
       isFpp.current = !isFpp.current;
 
       if (isFpp.current) {
-        cameraPosition.current.position.set(0, 3.5, -0.2);
+        cameraPosition.current.position.set(0, 3.3, -0.2);
         cameraTarget.current.position.set(0, 3.1, -1.5);
         if (mainCameraRef.current) {
           mainCameraRef.current.fov = 125;
