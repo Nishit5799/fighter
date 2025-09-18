20296a35e85aa5d7b4279948c462612a1363455c
Nishit Lugun <lugun.nishit@gmail.com>
2025-09-19 02:20:12 +0530

    nmcxjkds

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -967,7 +967,7 @@ const Experience = () => {
       </KeyboardControls>
       {showWelcomeScreen && (
         <div
-          className="fixed font-[Bebas] inset-0 flex items-center justify-center bg-black/50  z-50 start"
+          className="fixed font-[Bebas] inset-0 flex items-center justify-center bg-black/10  z-50 start"
           onClick={(e) => {
             if (!hasTappedToBegin) {
               e.stopPropagation();
