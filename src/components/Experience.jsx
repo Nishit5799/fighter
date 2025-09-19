b97531bb214a2b4496d9de82ca19bd78da5da530
Nishit Lugun <lugun.nishit@gmail.com>
2025-09-20 02:50:34 +0530

    nmcxjhkds

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1247,46 +1247,44 @@ const Experience = () => {
         }
       />
 
-      {isGameStarted && (
-        <button
-          onClick={() => {
-            const audio = bgMusicRef.current;
-            if (!audio) return;
+      <button
+        onClick={() => {
+          const audio = bgMusicRef.current;
+          if (!audio) return;
 
-            if (isMusicPlaying) {
-              audio.pause();
-              audio.currentTime = 0;
-            } else {
-              audio.currentTime = 0;
-              audio.play().catch(() => {});
-            }
+          if (isMusicPlaying) {
+            audio.pause();
+            audio.currentTime = 0;
+          } else {
+            audio.currentTime = 0;
+            audio.play().catch(() => {});
+          }
 
-            setIsMusicPlaying(!isMusicPlaying);
-          }}
-          className="fixed top-[30%] right-3 z-[9999] p-2 bg-black/60 rounded-full text-white"
-        >
-          {isMusicPlaying ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="w-6 h-6"
-            >
-              <path d="M11 5L6 9H2v6h4l5 4V5z" />
-              <path d="M15.54 8.46a5 5 0 010 7.07M19.07 4.93a9 9 0 010 12.73" />
-            </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="w-6 h-6"
-            >
-              <path d="M9.25 9.25L4 14.5h4l5 4V5l-5 4H4l5.25 5.25M16 12h.01M19 9l-3 3 3 3" />
-            </svg>
-          )}
-        </button>
-      )}
+          setIsMusicPlaying(!isMusicPlaying);
+        }}
+        className="fixed top-[30%] right-3 z-[9999] p-2 bg-black/60 rounded-full text-white"
+      >
+        {isMusicPlaying ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6"
+          >
+            <path d="M11 5L6 9H2v6h4l5 4V5z" />
+            <path d="M15.54 8.46a5 5 0 010 7.07M19.07 4.93a9 9 0 010 12.73" />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6"
+          >
+            <path d="M9.25 9.25L4 14.5h4l5 4V5l-5 4H4l5.25 5.25M16 12h.01M19 9l-3 3 3 3" />
+          </svg>
+        )}
+      </button>
 
       <audio ref={bgMusicRef} src="/bgmusic.mp3" loop preload="auto" />
     </>
