f77497c94585efcf3b67cbff7d6a3d051ee1eee3
Nishit Lugun <lugun.nishit@gmail.com>
2025-09-20 01:23:32 +0530

    nmcjkldsioew

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,6 +16,9 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Fight Arena",
   description: "Fight till you die",
+  icons: {
+    icon: "/favicon.png", // or "/favicon.png"
+  },
 };
 
 export default function RootLayout({ children }) {
